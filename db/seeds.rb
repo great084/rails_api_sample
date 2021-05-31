16750c38dba8fbe449a6a19afdf812b20ab031ec
great084 <great084@gmail.com>
2021-05-31 17:19:03 +0900

    testデータの削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,33 +9,33 @@ user_params = [
   }
 ]
 
-User.delete_all
+User.destroy_all
 User.create!(user_params)
 puts 'ユーザーの初期データの投入に成功しました!'
 
 user = User.find_by(email: 'test@example.com')
 
-post_params = [
-  {
-    user_id: 1,
-    title: 'React',
-    content: 'ユーザインターフェース構築のための JavaScript ライブラリ'
-  },
-  {
-    user_id: 1,
-    title: 'Vue.js',
-    content: 'The Progressive JavaScript Framework'
-  },
-  {
-    user_id: 1,
-    title: 'Angular',
-    content: 'モバイルとデスクトップ，ひとつのフレームワーク'
-  }
-]
+# post_params = [
+#   {
+#     user_id: 1,
+#     title: 'React',
+#     content: 'ユーザインターフェース構築のための JavaScript ライブラリ'
+#   },
+#   {
+#     user_id: 1,
+#     title: 'Vue.js',
+#     content: 'The Progressive JavaScript Framework'
+#   },
+#   {
+#     user_id: 1,
+#     title: 'Angular',
+#     content: 'モバイルとデスクトップ，ひとつのフレームワーク'
+#   }
+# ]
 
-Post.delete_all
-Post.create!(post_params)
-puts 'Postの初期データの投入に成功しました!'
+# Post.delete_all
+# Post.create!(post_params)
+# puts 'Postの初期データの投入に成功しました!'
 
 books_params = [
   {
